7d0bfc1f9d81bcf5aa80f885e2c36da97738d7f9
NJOlAI <37351546+PrimeClet@users.noreply.github.com>
2022-10-02 20:31:56 +0100

    nuxt config modifications generate side

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   loading: '~/components/splash.vue',
   ssr: true,
-  target: 'server',
+  target: 'static',
 
   router: {
     middleware: 'colorMode'
