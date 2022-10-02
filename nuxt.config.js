91cb1805cce7dfbbe678c11bfd9a257d8a057e77
NJOlAI <37351546+PrimeClet@users.noreply.github.com>
2022-10-02 19:55:13 +0100

    nuxt config modifications generate side

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   loading: '~/components/splash.vue',
-  ssr: true,
+  // ssr: false,
   target: 'static',
 
   // router: {
