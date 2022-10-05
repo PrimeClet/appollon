b832b7e69e4d88158ccbf711abe1c837a2339835
NJOlAI <37351546+PrimeClet@users.noreply.github.com>
2022-10-05 23:47:17 +0100

    nuxt small fetch comment

--- pages/login.vue
+++ pages/login.vue
@@ -153,13 +153,13 @@ import Lottie from 'vue-lottie/src/lottie.vue'
 
 
 export default {
-  asyncData({req, store}) {
-    if (process.server) {
-      const ip = req.headers['x-forwarded-for']
-      store.commit('PUT_USERIP', ip)
-      return { ipadd: ip }
-    }
-  },
+  // asyncData({req, store}) {
+  //   if (process.server) {
+  //     const ip = req.headers['x-forwarded-for']
+  //     store.commit('PUT_USERIP', ip)
+  //     return { ipadd: ip }
+  //   }
+  // },
   components: {
     lottie: Lottie,
     VueTelInput
